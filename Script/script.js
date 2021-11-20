1b5f0678b6cfbf63f20ede3807aa4a3feee1f0f8
Mattias Gaspar <mattiasmac2000@gmail.com>
2021-11-20 16:05:49 -0300

    Initial commit

---
+++ Script/script.js
@@ -0,0 +1,23 @@
+equal.addEventListener('click', result)
+delet.addEventListener('click', del)
+clear.addEventListener('click', cle)
+
+function insert(num) {
+        document.form.textview.value = document.form.textview.value + num
+}
+
+function result() {
+    let res = document.form.textview.value
+    if(res) {
+        document.form.textview.value = eval(res)
+    }
+}
+
+function del() {
+    let res = document.form.textview.value
+    document.form.textview.value = res.substring(0, res.length-1)
+}
+
+function cle() {
+    document.form.textview.value = ''
+}
